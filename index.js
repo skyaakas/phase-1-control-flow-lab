203777269496049b8b665cc6fa7e06e2736beef6
+ <+>
2022-06-27 16:54:50 -0600

    commit tree 77856e37ad6a70d7587af2c313aef3ec1d447189

--- index.js
+++ index.js
@@ -1,11 +1,53 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(a){
   // Write your code here!
+let message;
+  if (a < 400 ){
+  message = 'This one is on me!';
+  }
+  else if(a>400 && a<2000){
+    message = 'That will be twenty bucks.';
+  }
+  else if(a>2000 && a<2500){
+    message = 'I will gladly take your thirty bucks.';
+  }
+  else if(a>2500){
+    message = 'No can do.';
+  }
+  return message;
+}
+scuberGreetingForFeet(199);
+scuberGreetingForFeet(1500);
+scuberGreetingForFeet(2001);
+scuberGreetingForFeet(2501);
+
+function ternaryCheckCity(b){
+  // Write your code here!
+  let accept = b === 'NYC'? 'Ok, sounds good.': 'No go.';
+
+   return accept;
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+ternaryCheckCity('NYC');
 
-function switchOnCharmFromTip(){
+
+function switchOnCharmFromTip(c){
   // Write your code here!
+  let recall;
+
+  switch(c){
+    case 'generous':
+      recall = 'Thank you so much.';
+      break;
+
+    case 'not as generous':
+      recall = 'Thank you.';
+      break;
+    default : 
+    recall = 'Bye.';
+    break;
+
+  }
+  return recall;
 }
+switchOnCharmFromTip('generous');
